39a7fee4ef25e2ea9b17282a370f3ccbd57b4086
marcelomorales <lmoral10@jhu.edu>
2022-04-10 11:13:31 -0400

    clean up sql

---
+++ setup.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Video (
+  videoId int NOT NULL,
+  username VARCHAR(255),
+  caption VARCHAR(255),
+  comments VARCHAR(255),
+  PRIMARY KEY(videoId)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/video.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE UserProfile (
+  username VARCHAR(255),
+  userBio VARCHAR(255),
+  verified VARCHAR(255), 
+  followerCount int NOT NULL,
+  PRIMARY KEY(username)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/userprofile.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE Song (
+  audioId int NOT NULL,
+  title VARCHAR(255),
+  artist VARCHAR(255), 
+  PRIMARY KEY(audioId)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/song.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE Artist (
+  artistName VARCHAR(255),
+  audioId int NOT NULL,
+  PRIMARY KEY(artistName)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/artist.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE Album (
+  albumName VARCHAR(255),
+  artistName VARCHAR(255),
+  releaseYear int NOT NULL,
+  PRIMARY KEY(albumName, artistName)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/album.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+CREATE TABLE Revenue (
+  audioId VARCHAR(255),
+  amount DOUBLE,
+  PRIMARY KEY(audioId)
+);
+
+LOAD DATA LOCAL INFILE '/Users/marcelomorales/Desktop/SPRING2022JHU/databases/tiktok-database/revenue-small.txt'
+INTO TABLE Video
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+
