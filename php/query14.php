2dd18d62b805f6181e6b76937b33b6148760b9ce
marcelomorales <lmoral10@jhu.edu>
2022-05-04 10:37:50 -0400

    updates to main html page and php connections

---
+++ php/query14.php
@@ -0,0 +1,30 @@
+<head>
+    <title>Out of profiles that are verified in TikTok, list the username and number of likes for each of their TikTok videos  </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    include 'open.php';
+
+    echo "<h2> Who is verified and how many likes do they have from TikTok videos trending in December 2020 </h2><br>";
+
+    if ($result = $conn->query("CALL Query14();")) {
+        echo "<table class=\"table table-striped\">";
+        echo "<thead>";
+        echo "<tr><th>Profile username</th><th>Like count</th></tr>";
+        echo "</thead><tbody>";
+        foreach ($result as $row) {
+            echo "<tr>";
+            echo "<td>" . $row["username"] . "</td>";
+            echo "<td>" . $row["likeNumber"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        echo "Could not connect to the database.";
+    }
+    $conn->close();
+    ?>
+</body>
