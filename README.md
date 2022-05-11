9e506861ccc68c25bfb3cce2c58361d6dea282a5
Peter Novello <peternovello@Peters-MacBook-Pro-2.local>
2022-05-10 21:15:02 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# tiktok-database
+Marcelo Morales - lmoral10
+Peter Novello - pnovell1
+
+# Comments and Concerns
+
+During Phase C of the project, we explained to our CA mentor that we were unable to find reliable data to support
+our ambitions of performing queries on our data based on the revenue a song featured on TikTok has generated. Thus,
+we decided to use websites explaining the estimation of how much revenue can be generated based on the number of streams
+a song receives on a major platform, such as Spotify. We then estimated the revenue each song could have made and paired the revenues with the specific audio id for each of the songs looked at in our database. We hope that this estimation can 
+still allow us to bring in our economic domain of interest and compare the popularity, likes, and other characteristics of a 
+tiktok video against certain revenue indicators to make the project more interesting. 
+
+Updates for Phase D: We were able to run interesting queries based on the data we have populated in our tables. We will
+keep brainstorming to see if we can answer even more interesting questions for the final phase of the project. For the moment, no modifications are needed.
+
+Updates for Phase E: We were able to succesfully implement graphs and devise more creative queries with our data sets. 
+
+# Run set up script
+
+To run the set up script, make you are in the most outmost directory and run 
+  ```bash
+python set_up_tiktok_info.py 
+```
+so that your txt files can be filled with data.
+Once you are connected to the appropriate database, ensure that you run setup.sql
---
+++ queries.sql
@@ -0,0 +1,118 @@
+-- Query 1
+-- Which song made the least amount of revenue in the timeframe of December 2020?
+SELECT Song.title, Song.artist, MIN(Revenue.amount)
+    FROM Song, Revenue
+    WHERE Song.audioId = Revenue.audioId;
+
+-- Query 2
+-- What is the average follower count for profiles that had trending TikTok videos in December 2020?
+SELECT AVG(UserProfile.followerCount)
+    FROM UserProfile as U, Video as V
+    WHERE UserProfile.username = Video.username;
+
+-- Query 3
+-- “Which artists have the most trending songs from TikTok in December 2020?
+SELECT Artist.artist
+    COUNT (Artist.artist) as 'frequency'
+    FROM Artist
+    GROUP BY Artist.artist
+    ORDER BY 'frequency' DESC 
+    LIMIT 1;
+
+--  Query 4
+--  “What album has the most trending songs in TikTok in December 2020?”
+SELECT Album.albumName
+    COUNT (Album.albumName) as 'frequency'
+    FROM Album
+    GROUP BY Album.albumName
+    ORDER BY 'frequency' DESC 
+    LIMIT 1;
+
+--  Query 5
+--  “Which songs that are not part of an album, have just been released as singles, and have made a revenue greater than $10,000?”
+SELECT Song.title
+    FROM Song, Revenue
+    WHERE Revenue.amount > 10000 AND Song.albumName = NULL
+    GROUP BY Song.amount ;
+
+--  Query 6
+--  How many comments does the TikTok video with the most revenue have?
+
+WITH maxVideo AS (
+    SELECT MAX(Revenue.amount) AS 'highest', Video.videoId AS 'maxVideo'
+    FROM Revenue JOIN Video ON Revenue.audioID = Video.videoId
+)
+SELECT Video.comments
+FROM Video, maxVideo
+WHERE Video.comments = maxVideo.highest AND Video.videoId = maxVideo.maxVideo;
+
+
+--  Query 7
+-- What is the average follower count for profiles that had trending TikTok videos in December 2020?
+SELECT UserProfile.username, AVG(UserProfile.followerCount)
+    FROM UserProfile, Video
+    WHERE UserProfile.username = Video.username; 
+
+-- Query 9
+-- List the caption and sound/song used for the TikTok video that has the longest caption from all unverified users
+SELECT Video.caption, Song.title
+    FROM Video, Song
+    WHERE Video.audioId = Song.audioId 
+    ORDER BY LEN(Video.caption) DESC
+    LIMIT 1;
+
+-- Query 10
+-- How many profiles were unverified and did not utilize hashtags in their caption from the trending TikTok video dataset?
+SELECT UserProfile.username
+    FROM UserProfile, Video
+    WHERE UserProfile.verified  LIKE '%False%' AND
+    Video.caption NOT IN '%#%' AND UserProfile.username = Video.username ;
+
+-- Query 11
+-- What is the mean like number for a TikTok video with more than one hashtag
+SELECT Video.caption, AVG(Video.likeNumber)
+    FROM Video
+    WHERE Video.caption LIKE '%#%';
+
+
+-- Query 12
+-- “Out of original TikTok video audios, list the username and caption with captions that are longer than 50 characters?
+
+SELECT Video.username, Video.caption
+FROM Video
+WHERE LENGTH(Video.caption) > 50
+GROUP BY Video.username;
+
+
+-- Query 13
+ -- "Out of profiles that are verified in TikTok, list the minimum number of followers and maximum numbers of likes, as well as the associated userID"
+
+SELECT UserProfile.userName, MIN(UserProfile.followerCount), MAX(UserProfile.likeNumber)
+FROM UserProfile
+WHERE UserProfile.verified LIKE 'True';
+
+-- Query 14
+
+-- "Out of profiles that are verified in TikTok, list the username and number of comments for each of their TikTok Videos from December 2020. 
+
+SELECT UserProfile.userName, Video.comments
+FROM UserProfile JOIN Video ON UserProfile.userName = Video.userName
+WHERE UserProfile.verified LIKE '%True%'
+GROUP BY UserProfile.userName;
+
+
+-- Query 15
+
+--"For the artist who made the most revenue from TikTok videos in December 2020, list each of their audios in order from least to greatest revenue"
+
+
+WITH maxArtist AS (
+    SELECT MAX(Revenue.amount) AS 'highest', Artist.audioID AS 'maxArtist'
+    FROM Revenue NATURAL JOIN Artist
+)
+SELECT Song.title 
+FROM Song JOIN maxArtist ON
+     Song.audioID = maxArtist.maxArtist
+     JOIN Revenue ON
+     Song.audioID = Revenue.audioID
+ORDER BY Revenue.amount ASC;
